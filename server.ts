64b69241978ebd1cc1d528954fb197632217d471
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2025-05-06 20:36:36 +0530

    Intro to ZOD  Library

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import { pingHandler } from './controllers/ping.controller';
 import pingRouter from './routers/v1/ping.router';
 import v1Router from './routers/v1/index.router';
 import v2Router from './routers/v2/index.router';
+import { z } from 'zod';
 
 const app = express();
 
@@ -21,4 +22,19 @@ app.use('/api/v2', v2Router);
 app.listen(serverConfig.PORT, () => {
     console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
     console.log(`Press ctrl+C to stop the server`);    
+
+
+    //incoming data from the from value that want to test the zod schemas
+    const obj = {
+        name: "sanket",
+        age: 20,
+    }
+
+    //zod is a simple library that can be used to validate the incoming data from the form based on your expectations
+    const objSchema = z.object({
+        name: z.string(),
+        age: z.number().int().positive(),
+    });
+    const result = objSchema.parse(obj);
+    console.log(result);
 });
